5ae0f9101b3ac63ffdb70796137dbbd784936b5a
Akihiro Yamada <u.parr917@gmail.com>
2018-10-28 17:03:47 +0900

    implement simple tweet.

--- akiTweetBot.py
+++ akiTweetBot.py
@@ -1,4 +1,28 @@
 # -*- coding: utf-8 -*-
 
+# akiTweetBot.py
+# Created by Akihiro Yamada on 2018/10/27.
+# Copyright (c) 2018. All Rights Reserved.
+
 import twitter
 
+configFileName = '../.ApiConfig'
+confingFile = open(configFileName)
+
+lines = confingFile.read()
+
+keys = lines.split(",")
+
+consumerKey = str(keys[0])
+consumerSecret = str(keys[1])
+token = str(keys[2])
+tokenSecret = str(keys[3])
+
+
+# keyとtokenを設定。
+api = twitter.api.Api(consumer_key=consumerKey, 
+                     consumer_secret=consumerSecret, 
+                     access_token_key=token, 
+                     access_token_secret=tokenSecret)
+
+api.PostUpdates("テスト投稿!")
